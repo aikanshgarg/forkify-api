f44e22b3674dadec2b4e7c2e3da4cd6de133ad2f
root <aikanshgarg0303@gmail.com>
2019-03-21 15:23:41 +0530

    search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios'; // include the package
+import Search from './models/Search';
 
-async function getResults(query) {
-	const key = 'ffc8d69c30962a2817b4094b238c8924';
-	try {
-		const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-		const rec = res.data.recipes;
-		console.log(rec);
-	} catch (error){
-		alert(error);
-  	}
-}
-getResults('pizza');
-
-// api key: 
-// https://www.food2fork.com/api/search
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios'; // include the axios package: http protocol
+
+// ES6 class
+export default class Search {
+	constructor(query) {
+		this.query = query;
+	}
+
+	// method of Search class 
+	async getResults() {
+		const key = 'ffc8d69c30962a2817b4094b238c8924';
+		try {
+			const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+			this.result = res.data.recipes;
+			console.log(this.result);
+		} catch(error) {
+			alert(error);
+	  	}
+	}
+}
+
