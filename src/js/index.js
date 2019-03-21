b77b1a40a613be6687a149886ded8294a8d715af
root <aikanshgarg0303@gmail.com>
2019-03-21 12:39:30 +0530

    MVC structure setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! Variable x is ${x}`);
+import str from './models/Search';
+
+// import {add, multiply, ID} from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(`using imported fns! ${searchView.add(searchView.ID, 0)} and ${searchView.multiply(searchView.ID, 2)}. str: ${str}`);
