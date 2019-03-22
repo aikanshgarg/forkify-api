781a0b57227cca1a336afa83e4075477193b1d2e
root <aikanshgarg0303@gmail.com>
2019-03-22 08:13:27 +0530

    search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,40 @@
+// CONTROLLER: all in one file
+
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/** Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list onject
+* - Liked recipes
+*/
+
+const state = {};
+
+
+// method for search form
+const controlSearch = async () => { // async function for getting recipes from API
+	// 1) get query from view
+	const query = 'pizza';
+
+	if (query) {
+		// 2) New search object and add it to state
+		state.search = new Search(query);
+
+		// 3) Prepare UI for results (loading spinner)
+
+		// 4) Seatch for recipes
+		await state.search.getResults(); // getResults is an async fn which returns a promise, thus, await the result
+
+		// 5) Render results on UI
+		console.log(state.search.result);
+	}
+
+}
+
+// addEventListener on search form
+document.querySelector('.search').addEventListener('submit', e => {
+	e.preventDefault();
+	controlSearch();
+})
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -6,13 +6,13 @@ export default class Search {
 		this.query = query;
 	}
 
-	// method of Search class 
+	// async method of Search class (every async fn returns a promise) 
 	async getResults() {
 		const key = 'ffc8d69c30962a2817b4094b238c8924';
 		try {
 			const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
 			this.result = res.data.recipes;
-			console.log(this.result);
+			//console.log(this.result);
 		} catch(error) {
 			alert(error);
 	  	}
